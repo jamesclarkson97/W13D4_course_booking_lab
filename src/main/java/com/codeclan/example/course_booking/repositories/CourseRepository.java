73522a08ef199777dbefc6221d459723cfb1cc5b
James Clarkson <jamesiclarkson@yahoo.co.uk>
2020-09-10 11:36:20 +0100

    can find courses with a given rating

--- src/main/java/com/codeclan/example/course_booking/repositories/CourseRepository.java
+++ src/main/java/com/codeclan/example/course_booking/repositories/CourseRepository.java
@@ -4,6 +4,10 @@ import com.codeclan.example.course_booking.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.*;
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByRating(int i);
 }
--- src/test/java/com/codeclan/example/course_booking/CourseBookingApplicationTests.java
+++ src/test/java/com/codeclan/example/course_booking/CourseBookingApplicationTests.java
@@ -1,13 +1,38 @@
 package com.codeclan.example.course_booking;
 
+import com.codeclan.example.course_booking.models.Course;
+import com.codeclan.example.course_booking.repositories.BookingRepository;
+import com.codeclan.example.course_booking.repositories.CourseRepository;
+import com.codeclan.example.course_booking.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.awt.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CourseBookingApplicationTests {
 
+	@Autowired
+	BookingRepository bookingRepository;
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canFindCoursesWithRatingOf5() {
+		List<Course> foundCourse = courseRepository.findByRating(5);
+		assertEquals(1, foundCourse.size());
 	}
 
 }
